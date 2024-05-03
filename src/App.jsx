127a16c617ef7fd0f4860f4b4387753d821e49c0
AlokVerma89 <alokverma5712@gmail.com>
2024-05-03 09:52:50 +0530

    final Project done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BookStore from './assets/BookStore'
+
+function App() {
+  return (
+    <div>
+        <h1><center>BESTSELLER BOOKS</center></h1>
+      <BookStore/>
+    </div>
+  )
+}
+
+export default App
